a3659ba97505cc0059f31ce3d557f5f8def9d1b0
Generalkidd <Plasmadragons@hotmail.com>
2016-08-09 16:00:18 -0400

    Added Forge Menu
    Not fully working yet

---
+++ Assets/Scripts/MenuSelect.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSelect : MonoBehaviour
+{
+    string category = "";
+    
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnSelect()
+    {
+        category = this.GetComponent<TextMesh>().text;
+        GameObject tmp = GameObject.Find("MenuTitle");
+        tmp.GetComponent<MenuManager>().gameType = category;
+    }
+}
